68dbaf99c8e60e413fdb9ac1a3edcbf4c25a9875
Julczan <julek111222h@gmail.com>
2026-05-19 22:14:54 +0200

    show different page for different url

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const indexPath = path.resolve("index.html");
+const aboutPath = path.resolve("about.html");
+const contactPath = path.resolve("contact-me.html");
+const errorPath = path.resolve("404.html");
 
 const eventEmitter = new EventEmitter();
 
@@ -27,14 +30,47 @@ http
 
         response.writeHead(200, { "Content-Type": "text/html" });
 
-        fs.readFile(indexPath, "utf8", (err, data) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
+        switch (url) {
+          case "/":
+            fs.readFile(indexPath, "utf8", (err, data) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
 
-          response.end(data);
-        });
+              response.end(data);
+            });
+            break;
+
+          case "/about":
+            fs.readFile(aboutPath, "utf8", (err, data) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              response.end(data);
+            });
+            break;
+
+          case "/contact-me":
+            fs.readFile(contactPath, "utf8", (err, data) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              response.end(data);
+            });
+            break;
+          default:
+            fs.readFile(errorPath, "utf8", (err, data) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              response.end(data);
+              return;
+            });
+        }
       });
   })
   .listen(8080);
